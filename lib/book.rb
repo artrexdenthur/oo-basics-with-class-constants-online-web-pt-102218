cc32cf2bf80ecd41176d16a471598cc21314fa3c
Paul Ashour <paul.ashour@gmail.com>
2018-11-11 07:07:58 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,5 @@
 class Book
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :author, :page_count, :genres
   attr_reader :title
 
   def initialize(title)
@@ -9,5 +9,7 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+  
+  
 
 end
